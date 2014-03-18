61ca57fca61417450ed07c6d8334b3cd5e93f390
petea <smart__petea@mail.ru>
2014-03-18 08:54:42 +0200

    added edit support for BookView

--- js/views.js
+++ js/views.js
@@ -11,6 +11,27 @@ var	BookViewTemplate = [
 		"<div class='buttons'><button class='edit'>Edit</button><button class='remove'>Remove</button></div>",
 ].join("");
 
+var BookEditTemplate = [
+	"<div class='inner'>",
+		"<p>",
+			"<label>Name: </label><input class='name' type='text' value='<%= name %>'>",
+		"</p>",
+		"<p>",
+			"<label>Price: </label><input class='price' type='text' value='<%= price %>'>",
+		"</p>",
+		"<p>",
+			"<label>Description: </label><textarea class='desc' type='text' ><%= desc %></textarea>",
+		"</p>",
+		"<p>",
+			"<label>Picture: </label><input class='picture' type='text' value='<%= picture %>'>",
+		"</p>",
+		"<p>",
+			"<button class='cancelButton'>Cancel</button>",
+			"<button class='okButton'>Ok</button>",
+		"</p>",
+	"</div>",
+].join("");
+
 var BookView = Backbone.View.extend({
 	template: _.template(BookViewTemplate),
 	initialize: function() {
@@ -23,11 +44,64 @@ var BookView = Backbone.View.extend({
 
 	events: {
 		'click .remove': 'removeItem',
+		'click .edit': 'editItem',
+	},
+
+	editItem: function() {
+		new BookEditView({
+			model: this.model,
+			top: this.$el.position().top,
+		});
 	},
 
 	removeItem: function() {
 		this.model.destroy();
 	},
+
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	},
+});
+
+var BookEditView = Backbone.View.extend({
+	tagName: 'div',
+	className: 'edit-view',
+	initialize: function(options) {
+		$('body').append(this.render().$el);
+		this.$el.css({
+			width: $('body').width(),
+			height: $('body').height(),
+		});
+		this.$('.inner').css({
+			top: options.top,
+		});
+	},
+	
+	events: {
+		'click .okButton': 'okClick',
+		'click .cancelButton': 'cancelClick',
+	},
+
+	cancelClick: function() {
+		this.$el.remove();
+	},
+
+	okClick: function() {
+		var price = this.$('.price').val(),
+			name = this.$('.name').val(),
+			desc = this.$('textarea').val(),
+			picture = this.$('.picture').val();
+		this.model.set({
+			price: price,
+			name: name,
+			desc: desc,
+			picture: picture,
+		}).save();
+		 
+		this.$el.remove();
+	}, 
+	template: _.template(BookEditTemplate),
 
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
